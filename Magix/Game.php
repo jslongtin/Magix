13c63b200408861636eb8da737db99587eec3243
jslongtin <jessikalongtin@hotmail.com>
2022-11-07 13:30:36 -0500

    avance css board

--- Magix/Game.php
+++ Magix/Game.php
@@ -10,30 +10,33 @@ require_once("partial/header.php");
 <div id="game">
 
 
-    <div id="enemy"></div>
-    <div id="board">
-        <div id="action-buttons">
-            <div id="message"></div>
-            <button name="heroPower" onclick="heroPower()">Hero Power</button>
-            <button name="endTurn" onclick="endTurn()">End Turn</button>
-            <button name="surrender" onclick="surrender()">Surrender</button>
-            <button name="quitter" onclick="quitGame()">Quitter</button>
-            <div id="timer"></div>
-            <!-- health bar -->
-            <div id="healthBar">
-                <div id="vies">0</div>
-                <progress id="health" value="100" max="100"> </progress>
-            </div>
-
-        </div>
-        <div id="boardCardContainer"></div>
+  <div id="enemy"></div>
+  <div id="board">
+    <div id="boardCardContainer"></div>
+  </div>
+  <div id="player">
+    <div id="info">
+      <div id="message"></div>
+      <div id="timer"></div>
+      <!-- health bar -->
+      <div id="healthBar">
+        <div id="vies">0</div>
+        <progress id="health" value="100" max="100"> </progress>
+      </div>
+      <div id="mana"></div>
+      <div id="turn"></div>
+      <div id="remaining"></div>
     </div>
-    <div id="mana"></div>
-    <div id="turn"></div>
-    <div id="remaining"></div>
     <div id="card-container"></div>
-    <!-- card exemple -->
-    <!-- <div class="card">
+    <div id="action-buttons">
+      <button name="heroPower" onclick="heroPower()">Hero Power</button>
+      <button name="endTurn" onclick="endTurn()">End Turn</button>
+      <button name="surrender" onclick="surrender()">Surrender</button>
+      <button name="quitter" onclick="quitGame()">Quitter</button>
+    </div>
+  </div>
+  <!-- card exemple -->
+  <!-- <div class="card">
   <img  alt="Carte" src="img/Cartes/1664932350_837161.png" style="width:100%">
   <div class="container">
     <h4><b>Name</b></h4>
